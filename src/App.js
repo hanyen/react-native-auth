052f352f5955e0c5053e92098d946a6c9f04f2f8
hanyen <me@hanyen.com>
2017-08-28 09:58:52 -0700

    Setup firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import firebase from 'firebase';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
 
 class App extends React.Component {
+  
+  componentWillMount() {
+    // Initialize Firebase
+    firebase.initializeApp({
+        apiKey: 'AIzaSyBSUa7erjf3OW9J-SkJ7iBSytNMkBr5TYM',
+        authDomain: 'authentication-69314.firebaseapp.com',
+        databaseURL: 'https://authentication-69314.firebaseio.com',
+        projectId: 'authentication-69314',
+        storageBucket: 'authentication-69314.appspot.com',
+        messagingSenderId: '465131003483'
+      });
+  }
+
   render() {
     return (
       <View>
